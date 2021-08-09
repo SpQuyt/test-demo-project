b5f55eedda1742822cb1f838b27430f69aeb1f49
SpQuyt <spquyt@gmail.com>
2021-08-09 11:40:34 +0700

    feature/init base

---
+++ src/utilities/staticData.ts
@@ -0,0 +1,9 @@
+export const staticValue = {
+    DEFAULT: 1,
+    TIME_IMAGE_LOAD: 500,
+};
+
+export const ERRORS = {
+    default: 'common.error.unknown',
+    network: 'common.error.network',
+};
